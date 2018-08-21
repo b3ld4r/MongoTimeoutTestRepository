649b43751f78b6eac8159eb1ea2ec10ff6421e64
Daniel Alderete <daniel.alderetecillero@telefonica.com>
2018-08-21 13:41:14 +0200

    Modified way of accessing mongo config, added log and return options in controller

--- app/configuration/MongoConfig.java
+++ app/configuration/MongoConfig.java
@@ -1,14 +1,18 @@
 package configuration;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
+import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigObject;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.Morphia;
 
 import java.util.ArrayList;
 import java.util.List;
+import play.Logger;
 
 public class MongoConfig {
 
@@ -22,15 +26,51 @@ public class MongoConfig {
         return datastore;
     }
 
-    public static void initDatastore() {
+    private static void initDatastore() {
 
         final Morphia morphia = new Morphia();
 
         morphia.mapPackage("app.models");
+        MongoClient mongoClient;
 
-        MongoClient mongoClient = new MongoClient(new MongoClientURI(ConfigFactory.load().getString("mongodb.uri")));
+        if (ConfigFactory.load().getString("mongodb.mode").compareTo("list") == 0) {
 
+            Logger.info("MongoConfig.initDatastore: Mode is list");
+            List<ServerAddress> serverAddressList = new ArrayList<>();
+
+            Logger.info("MongoConfig.initDatastore: Reading hosts");
+            List<? extends ConfigObject> list = ConfigFactory.load().getObjectList("mongodb.server_addresses");
+
+            if (list != null && !list.isEmpty()) {
+                for (ConfigObject object : list) {
+                    Config tmp = object.toConfig();
+                    serverAddressList.add(new ServerAddress(tmp.getString("host"), tmp.getInt("port")));
+                    Logger.info("MongoConfig.initDatastore: Host: {}, port: {}", tmp.getString("host"), tmp.getInt("port"));
+                }
+            }
+
+            Logger.info("MongoConfig.initDatastore: Setting options");
+            MongoClientOptions.Builder clientOptions = new MongoClientOptions.Builder();
+            clientOptions.connectionsPerHost(ConfigFactory.load().getInt("mongodb.max_pool_size"));
+            Logger.info("MongoConfig.initDatastore: Max pool size is: {}", ConfigFactory.load().getInt("mongodb.max_pool_size"));
+
+            Logger.info("MongoConfig.initDatastore: Creating client");
+            mongoClient = new MongoClient(serverAddressList, clientOptions.build());
+            Logger.info("MongoConfig.initDatastore: Client created");
+        } else if (ConfigFactory.load().getString("mongodb.mode").compareTo("uri") == 0) {
+
+            Logger.info("MongoConfig.initDatastore: Mode is uri");
+            Logger.info("MongoConfig.initDatastore: Creating client");
+            mongoClient = new MongoClient(new MongoClientURI(ConfigFactory.load().getString("mongodb.uri")));
+            Logger.info("MongoConfig.initDatastore: Client created");
+        } else {
+            Logger.info("MongoConfig.initDatastore: No mode supplied in mongo configuration");
+            throw new RuntimeException("No mode supplied in mongo configuration");
+        }
+
+        Logger.info("MongoConfig.initDatastore: Creating datastore");
         datastore = morphia.createDatastore(
                 mongoClient, ConfigFactory.load().getString("mongodb.db_name"));
+        Logger.info("MongoConfig.initDatastore: Datastore created");
     }
 }
--- app/controllers/KeyboardController.java
+++ app/controllers/KeyboardController.java
@@ -5,34 +5,94 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import models.Keyboard;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.Logger;
+
+import java.util.List;
 
 public class KeyboardController extends Controller {
 
     public Result createKeyboard(String brand) {
+
+        Logger.info("KeyboardController.createKeyboard: brand is {}", brand);
+
+        if (brand == null || brand.isEmpty()) {
+            Logger.error("KeyboardController.createKeyboard: empty brand");
+            return badRequest("Empty brand");
+        }
+
         Keyboard keyboard = new Keyboard(brand);
+
+        Logger.info("KeyboardController.createKeyboard: generated id is {}", keyboard.getId());
+
         return created((JsonNode) new ObjectMapper().valueToTree(keyboard));
     }
 
     public Result getAll() {
-        return ok((JsonNode) new ObjectMapper().valueToTree(Keyboard.getAll()));
+
+        List<Keyboard> keyboards = Keyboard.getAll();
+
+        if (keyboards == null || keyboards.isEmpty()) {
+            Logger.error("KeyboardController.getAll: no keyboards found in database");
+            return notFound("No keyboards found in database");
+        } else {
+            Logger.info("KeyboardController.getAll: returning keyboards found in database");
+            return ok((JsonNode) new ObjectMapper().valueToTree(Keyboard.getAll()));
+        }
     }
 
     public Result updateKeyboard(Long id, String brand) {
 
+        Logger.info("KeyboardController.updateKeyboard: id is {}, brand is {}", id, brand);
+
+        if (brand == null || brand.isEmpty()) {
+            Logger.error("KeyboardController.updateKeyboard: empty brand");
+            return badRequest("Empty brand");
+        }
+
+        if (id == null) {
+            Logger.error("KeyboardController.updateKeyboard: empty id");
+            return badRequest("Empty id");
+        }
+
         Keyboard keyboard = Keyboard.getById(id);
+
+        if (keyboard == null) {
+            Logger.error("KeyboardController.updateKeyboard: keyboard id not found in database");
+            return notFound("Keyboard id not found in database");
+        }
+
+        Logger.info("KeyboardController.updateKeyboard: updating keyboard");
         keyboard.update(brand);
         return ok((JsonNode) new ObjectMapper().valueToTree(keyboard));
     }
 
     public Result getKeyboard(Long id) {
 
+        Logger.info("KeyboardController.getKeyboard: id is {}", id);
+
         Keyboard keyboard = Keyboard.getById(id);
+
+        if (keyboard == null) {
+            Logger.error("KeyboardController.getKeyboard: keyboard id not found in database");
+            return notFound("Keyboard id not found in database");
+        }
+
+        Logger.info("KeyboardController.getKeyboard: keyboard found in database");
         return ok((JsonNode) new ObjectMapper().valueToTree(keyboard));
     }
 
     public Result deleteKeyboard(Long id) {
 
+        Logger.info("KeyboardController.deleteKeyboard: id is {}", id);
+
         Keyboard keyboard = Keyboard.getById(id);
+
+        if (keyboard == null) {
+            Logger.error("KeyboardController.createKeyboard: keyboard id not found in database");
+            return notFound("Keyboard id not found in database");
+        }
+
+        Logger.info("KeyboardController.getKeyboard: keyboard found in database, deleting it");
         keyboard.delete();
         return noContent();
     }
